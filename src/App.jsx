4415c623f3294ee6020df833caff71c875c923d4
Danimal <danimal@hackpoetic.com>
2023-12-27 18:50:18 -0500

    feat: add schedule data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+// uuid
+import { v4 as uuid } from "uuid";
+
+// Ours - Styles
 import styles from "./App.module.css";
 
+// Ours - Data
+import dailies from "./data/dailies.json";
+
+// Add IDs to each daily
+dailies.forEach((daily) => {
+  daily.id = uuid();
+});
+
 function App() {
-  return <main className={styles["page"]}></main>;
+  return <main className={styles["page"]}>{JSON.stringify(dailies)}</main>;
 }
 
 export default App;
