a5af218392b28d98259ec577304754d787615419
Danimal <danimal@hackpoetic.com>
2024-02-12 09:15:44 -0500

    refactor: remove App.jsx in prep for NextJS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
+// React
 import React from "react";
+
+// React DOM
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
+// Ours - Styles
 import "./index.css";
+
+// Ours - Firebase
+import "./firebase";
+
+// Ours - Router
+import { router } from "./router";
+
+// React Router
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />;
   </React.StrictMode>
 );
