2f08ec0602966080e2f03fe7c4325671da8ab813
Danimal <danimal@hackpoetic.com>
2024-02-14 16:52:33 -0500

    feat: progress dashboard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,63 @@
-// React
-import { useId, useState } from "react";
-
-// React Router DOM
-import { Link } from "react-router-dom";
+// Hours - Hooks
+import { useAppData } from "@/hooks";
 
 // Ours - Styles
 import styles from "./page.module.css";
 
+// Ours - DB
+import { TaskStatus } from "@/db";
+
+// Ours - Components
+import { Progress } from "@/components/Progress";
+
 export default function Page() {
+  const { tasks } = useAppData();
+
+  const progressByGoal = tasks.reduce((acc, { goal, status, deleted }) => {
+    if (deleted) {
+      return acc;
+    }
+
+    const progress = acc[goal] || { total: 0, completed: 0 };
+
+    progress.total += 1;
+    if (status === TaskStatus.COMPLETED) {
+      progress.completed += 1;
+    }
+
+    return {
+      ...acc,
+      [goal]: progress,
+    };
+  }, {});
+
+  console.log(tasks);
+
   return (
     <>
       <section className={styles["hero"]}>
         <h1>Welcome!</h1>
         <p>Best of luck on your tasks, my friend.</p>
+      </section>
+      <section className={styles["progress-section"]}>
+        <h2>Progress</h2>
+        <div className={styles["goals"]}>
+          {Object.entries(progressByGoal).map(
+            ([goal, { total, completed }]) => (
+              <>
+                <div key={`title_${goal}`} className={styles["goal__title"]}>
+                  {goal}
+                </div>
+                <div
+                  key={`progress_${goal}`}
+                  className={styles["goal__progress"]}
+                >
+                  <Progress current={completed} total={total} />
+                </div>
+              </>
+            )
+          )}
+        </div>
       </section>
     </>
   );
